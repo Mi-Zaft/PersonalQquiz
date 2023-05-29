ff75b94ccee840c6d45edd47fc2dc095a7e1af18
MiZaft <mi.zaft1@gmail.com>
2023-05-29 22:52:14 +0300

    1)Array with result to result screen; 2)Get the most popular answer; 3)Show result via the most popular answer; 4)Hide "back" button on the result screen;

--- PersonalQquiz/ViewControllers/ResultViewController.swift
+++ PersonalQquiz/ViewControllers/ResultViewController.swift
@@ -9,17 +9,41 @@ import UIKit
 
 final class ResultViewController: UIViewController {
     
+    @IBOutlet var resultLabel: UILabel!
+    @IBOutlet var resultDescriptionLabel: UILabel!
+    
     var results: [Answer]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.hidesBackButton = true
-        findMostFrequentElement(in: results)
+        updateResultDate()
     }
     
     @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
         navigationController?.dismiss(animated: true)
+    }
+    
+    private func updateResultDate() {
+        let theMostPopularAnswer = mostFrequentElement(in: results)
+        
+        if theMostPopularAnswer!.animal == Animal.dog {
+            resultLabel.text = "Вы - \(Animal.dog.rawValue)"
+            resultDescriptionLabel.text = Animal.dog.definition
+            
+        } else if theMostPopularAnswer!.animal == Animal.cat {
+            resultLabel.text = "Вы - \(Animal.cat.rawValue)"
+            resultDescriptionLabel.text = Animal.cat.definition
+            
+        } else if theMostPopularAnswer!.animal == Animal.rabbit {
+            resultLabel.text = "Вы - \(Animal.rabbit.rawValue)"
+            resultDescriptionLabel.text = Animal.rabbit.definition
+            
+        } else if theMostPopularAnswer!.animal == Animal.turtle {
+            resultLabel.text = "Вы - \(Animal.turtle.rawValue)"
+            resultDescriptionLabel.text = Animal.turtle.definition
+        }
     }
     
     deinit {
@@ -29,16 +53,30 @@ final class ResultViewController: UIViewController {
 
 // MARK: - Private Methods
 private extension ResultViewController {
-    func findMostFrequentElement(in answers: [Answer]) {
-        var frequencyDict: [Animal: Int] = [:]
+    func mostFrequentElement(in array: [Answer]) -> Answer? {
+        var frequencyDict = [Answer: Int]()
         
-        for answer in answers {
-            if let animalCount = frequencyDict[answer.animal] {
-                frequencyDict.updateValue(animalCount + 1, forKey: answer.animal)
-            } else {
-                frequencyDict[answer.animal] = 1
+        for element in array {
+            frequencyDict[element, default: 0] += 1
+        }
+        
+        var maxFrequency = 0
+        var mostFrequentElement: Answer?
+        
+        for (element, frequency) in frequencyDict {
+            if frequency > maxFrequency {
+                maxFrequency = frequency
+                mostFrequentElement = element
             }
         }
-        print(frequencyDict)
+        
+        return mostFrequentElement
     }
+    
+    // Пример использования
+//    if let mostFrequent = mostFrequentElement(in: results) {
+//        print("Наиболее часто встречающийся элемент: \(mostFrequent)")
+//    } else {
+//        print("Массив пустой")
+//    }
 }
